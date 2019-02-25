69280477e90ed3dee024d601ae7b0a269813f30d
Niels van der Molen <nielsvandermolen@goalgorilla.com>
2019-02-25 17:27:10 +0100

    Step 5: CheeseListing: add a Doctrine DataFixture

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\CheeseListing;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $cheeseListing = new CheeseListing();
+        $cheeseListing->setTitle('Cheese Title');
+        $cheeseListing->setDescription('Cheese Description');
+        $cheeseListing->setCreated(new \DateTime());
+        $cheeseListing->setIsStinky(false);
+
+        $manager->persist($cheeseListing);
+
+        $manager->flush();
+    }
+}
