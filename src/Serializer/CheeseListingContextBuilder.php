cd4cf31dda573f139464a67a70d52f1a282762ea
Niels van der Molen <nielsvandermolen@bijlesaanbod.nl>
2019-03-25 16:57:15 +0100

    Step 15: SerialGroup; ContextBuilder boilerplate

---
+++ src/Serializer/CheeseListingContextBuilder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Serializer;
+
+use ApiPlatform\Core\Serializer\SerializerContextBuilderInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+final class CheeseListingContextBuilder implements SerializerContextBuilderInterface
+{
+    private $decorated;
+    private $authorizationChecker;
+
+    public function __construct(SerializerContextBuilderInterface $decorated, AuthorizationCheckerInterface $authorizationChecker)
+    {
+        $this->decorated = $decorated;
+        $this->authorizationChecker = $authorizationChecker;
+    }
+
+    public function createFromRequest(Request $request, bool $normalization, ?array $extractedAttributes = null): array
+    {
+        $context = $this->decorated->createFromRequest($request, $normalization, $extractedAttributes);
+        return $context;
+    }
+}
