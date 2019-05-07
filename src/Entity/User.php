011c6af21e980e12f4dba604c9b596d0139b695e
Niels van der Molen <nielsvandermolen@bijlesaanbod.nl>
2019-05-07 16:19:27 +0200

    Step 31: subresources

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiSubresource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -60,6 +61,7 @@ class User implements UserInterface
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\CheeseListing", mappedBy="owner")
      * @Groups({"user:output"})
+     * @ApiSubresource
      */
     private $cheeseListings;
 
