78b5e26a1459cf060da4daded91047363920b612
Niels van der Molen <nielsvandermolen@bijlesaanbod.nl>
2019-05-07 16:19:25 +0200

    Step 8: cheeselisting-operation-shortname

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -8,7 +8,8 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ApiResource(
  *     collectionOperations={"get", "post"},
- *     itemOperations={"get", "put"}
+ *     itemOperations={"get", "put"},
+ *     shortName="cheeses"
  * )
  * @ORM\Entity(repositoryClass="App\Repository\CheeseListingRepository")
  */
