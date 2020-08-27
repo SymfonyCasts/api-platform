2a9d1c92e849b6c0b8226d867affb2f645887419
Ryan Weaver <ryan@thatsquality.com>
2020-08-27 13:11:07 -0400

    [tuts-apply-diff] Applying existing diff file for step persisterdecoration-use-other-interface

--- src/DataPersister/UserDataPersister.php
+++ src/DataPersister/UserDataPersister.php
@@ -2,12 +2,12 @@
 
 namespace App\DataPersister;
 
-use ApiPlatform\Core\DataPersister\DataPersisterInterface;
+use ApiPlatform\Core\DataPersister\ContextAwareDataPersisterInterface;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-class UserDataPersister implements DataPersisterInterface
+class UserDataPersister implements ContextAwareDataPersisterInterface
 {
     private $entityManager;
     private $userPasswordEncoder;
@@ -18,7 +18,7 @@ class UserDataPersister implements DataPersisterInterface
         $this->userPasswordEncoder = $userPasswordEncoder;
     }
 
-    public function supports($data): bool
+    public function supports($data, array $context = []): bool
     {
         return $data instanceof User;
     }
@@ -26,7 +26,7 @@ class UserDataPersister implements DataPersisterInterface
     /**
      * @param User $data
      */
-    public function persist($data)
+    public function persist($data, array $context = [])
     {
         if ($data->getPlainPassword()) {
             $data->setPassword(
@@ -39,7 +39,7 @@ class UserDataPersister implements DataPersisterInterface
         $this->entityManager->flush();
     }
 
-    public function remove($data)
+    public function remove($data, array $context = [])
     {
         $this->entityManager->remove($data);
         $this->entityManager->flush();
