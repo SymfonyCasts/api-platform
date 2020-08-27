c5c444d16816f9b5209764c568f074bbbf15a132
Ryan Weaver <ryan@thatsquality.com>
2020-08-27 13:11:05 -0400

    [tuts-apply-diff] Applying existing diff file for step pre-ep3-install-doctrine-fixtures

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
