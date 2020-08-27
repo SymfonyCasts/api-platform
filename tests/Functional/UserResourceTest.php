22e4dc8d7f7853df065d2041155fe2fa85a6da1c
Ryan Weaver <ryan@thatsquality.com>
2020-08-27 13:10:52 -0400

    [tuts-apply-diff] Applying existing diff file for step dynamserialization-add-test-for-phonenumber-admin-only

--- tests/Functional/UserResourceTest.php
+++ tests/Functional/UserResourceTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Functional;
 
+use App\Entity\User;
 use App\Test\CustomApiTestCase;
 use Hautelook\AliceBundle\PhpUnit\ReloadDatabaseTrait;
 
@@ -38,6 +39,35 @@ class UserResourceTest extends CustomApiTestCase
         $this->assertResponseIsSuccessful();
         $this->assertJsonContains([
             'username' => 'newusername'
+        ]);
+    }
+
+    public function testGetUser()
+    {
+        $client = self::createClient();
+        $user = $this->createUserAndLogIn($client, 'cheeseplease@example.com', 'foo');
+
+        $user->setPhoneNumber('555.123.4567');
+        $em = $this->getEntityManager();
+        $em->flush();
+
+        $client->request('GET', '/api/users/'.$user->getId());
+        $this->assertJsonContains([
+            'username' => 'cheeseplease'
+        ]);
+
+        $data = $client->getResponse()->toArray();
+        $this->assertArrayNotHasKey('phoneNumber', $data);
+
+        // refresh the user & elevate
+        $user = $em->getRepository(User::class)->find($user->getId());
+        $user->setRoles(['ROLE_ADMIN']);
+        $em->flush();
+        $this->logIn($client, 'cheeseplease@example.com', 'foo');
+
+        $client->request('GET', '/api/users/'.$user->getId());
+        $this->assertJsonContains([
+            'phoneNumber' => '555.123.4567'
         ]);
     }
 }
