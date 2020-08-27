69ff38a692ff8b2e465ab1361fa7a190b05c0956
Ryan Weaver <ryan@thatsquality.com>
2020-08-27 13:10:54 -0400

    [tuts-apply-diff] Applying existing diff file for step normalizer-make-usernormalizer

---
+++ src/Serializer/Normalizer/UserNormalizer.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Serializer\Normalizer;
+
+use Symfony\Component\Serializer\Normalizer\CacheableSupportsMethodInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+class UserNormalizer implements NormalizerInterface, CacheableSupportsMethodInterface
+{
+    private $normalizer;
+
+    public function __construct(ObjectNormalizer $normalizer)
+    {
+        $this->normalizer = $normalizer;
+    }
+
+    public function normalize($object, $format = null, array $context = array()): array
+    {
+        $data = $this->normalizer->normalize($object, $format, $context);
+
+        // Here: add, edit, or delete some data
+
+        return $data;
+    }
+
+    public function supportsNormalization($data, $format = null): bool
+    {
+        return $data instanceof \App\Entity\BlogPost;
+    }
+
+    public function hasCacheableSupportsMethod(): bool
+    {
+        return true;
+    }
+}
