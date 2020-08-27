1ff96c91261c3bf1a2aa9f6dc0aaa173b8bbf9d9
Ryan Weaver <ryan@thatsquality.com>
2020-08-27 13:10:57 -0400

    [tuts-apply-diff] Applying existing diff file for step auto-owner-finish-logic

--- src/Doctrine/CheeseListingSetOwnerListener.php
+++ src/Doctrine/CheeseListingSetOwnerListener.php
@@ -3,10 +3,25 @@
 namespace App\Doctrine;
 
 use App\Entity\CheeseListing;
+use Symfony\Component\Security\Core\Security;
 
 class CheeseListingSetOwnerListener
 {
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     public function prePersist(CheeseListing $cheeseListing)
     {
+        if ($cheeseListing->getOwner()) {
+            return;
+        }
+
+        if ($this->security->getUser()) {
+            $cheeseListing->setOwner($this->security->getUser());
+        }
     }
 }
