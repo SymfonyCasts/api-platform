faf696326c9de34fa3575ce99f86241d4a9c25ea
Ryan Weaver <ryan@thatsquality.com>
2020-08-27 13:18:03 -0400

    tuts hero working...

---
+++ src/Dto/CheeseListingInput.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Dto;
+
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\SerializedName;
+
+class CheeseListingInput
+{
+    /**
+     * @Groups({"cheese:write", "user:write"})
+     */
+    public $title;
+
+    /**
+     * @Groups({"cheese:write", "user:write"})
+     */
+    public $price;
+
+    /**
+     * @Groups({"cheese:collection:post"})
+     */
+    public $owner;
+
+    public $description;
+
+    /**
+     * The description of the cheese as raw text.
+     *
+     * @Groups({"cheese:write", "user:write"})
+     * @SerializedName("description")
+     */
+    public function setTextDescription(string $description): self
+    {
+        $this->description = nl2br($description);
+
+        return $this;
+    }
+}
