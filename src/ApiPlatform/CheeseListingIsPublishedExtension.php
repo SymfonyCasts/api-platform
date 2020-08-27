bd1d18958a699322bd1dc2753a82cc7f478da938
Ryan Weaver <ryan@thatsquality.com>
2020-08-27 13:10:58 -0400

    [tuts-apply-diff] Applying existing diff file for step extension-empty-cheeselistingispublishedextension

---
+++ src/ApiPlatform/CheeseListingIsPublishedExtension.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\ApiPlatform;
+
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use Doctrine\ORM\QueryBuilder;
+
+class CheeseListingIsPublishedExtension implements QueryCollectionExtensionInterface
+{
+    public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
+    {
+
+    }
+}
