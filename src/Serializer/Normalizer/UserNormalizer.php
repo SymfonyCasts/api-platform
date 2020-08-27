cf3e1a764435bcb99f7d95c6a0305c0a61390197
Ryan Weaver <ryan@thatsquality.com>
2020-08-27 13:10:55 -0400

    [tuts-apply-diff] Applying existing diff file for step normalizer-fix-recursion

--- src/Serializer/Normalizer/UserNormalizer.php
+++ src/Serializer/Normalizer/UserNormalizer.php
@@ -4,13 +4,15 @@ namespace App\Serializer\Normalizer;
 
 use App\Entity\User;
 use Symfony\Component\Serializer\Normalizer\CacheableSupportsMethodInterface;
+use Symfony\Component\Serializer\Normalizer\ContextAwareNormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
-use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
-class UserNormalizer implements NormalizerInterface, CacheableSupportsMethodInterface, NormalizerAwareInterface
+class UserNormalizer implements ContextAwareNormalizerInterface, CacheableSupportsMethodInterface, NormalizerAwareInterface
 {
     use NormalizerAwareTrait;
+
+    private const ALREADY_CALLED = 'USER_NORMALIZER_ALREADY_CALLED';
 
     /**
      * @param User $object
@@ -21,6 +23,8 @@ class UserNormalizer implements NormalizerInterface, CacheableSupportsMethodInte
             $context['groups'][] = 'owner:read';
         }
 
+        $context[self::ALREADY_CALLED] = true;
+
         $data = $this->normalizer->normalize($object, $format, $context);
 
         // Here: add, edit, or delete some data
@@ -28,8 +32,13 @@ class UserNormalizer implements NormalizerInterface, CacheableSupportsMethodInte
         return $data;
     }
 
-    public function supportsNormalization($data, $format = null): bool
+    public function supportsNormalization($data, $format = null, array $context = [])
     {
+        // avoid recursion: only call once per object
+        if (isset($context[self::ALREADY_CALLED])) {
+            return false;
+        }
+
         return $data instanceof User;
     }
 
