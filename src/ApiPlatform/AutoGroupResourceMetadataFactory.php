397f65314027acae3039e34bd7b1b7e196673810
Ryan Weaver <ryan@thatsquality.com>
2020-08-27 13:10:53 -0400

    [tuts-apply-diff] Applying existing diff file for step autogroups-empty-resource-factory

---
+++ src/ApiPlatform/AutoGroupResourceMetadataFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\ApiPlatform;
+
+use ApiPlatform\Core\Metadata\Resource\Factory\ResourceMetadataFactoryInterface;
+use ApiPlatform\Core\Metadata\Resource\ResourceMetadata;
+
+class AutoGroupResourceMetadataFactory implements ResourceMetadataFactoryInterface
+{
+    private $decorated;
+
+    public function __construct(ResourceMetadataFactoryInterface $decorated)
+    {
+        $this->decorated = $decorated;
+    }
+
+    public function create(string $resourceClass): ResourceMetadata
+    {
+        $resourceMetadata = $this->decorated->create($resourceClass);
+
+        return $resourceMetadata;
+    }
+}
