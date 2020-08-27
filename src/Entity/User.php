39ab6e9ba30a7957f5d6f15a00ad036d6971b7ce
Ryan Weaver <ryan@thatsquality.com>
2020-08-27 13:10:51 -0400

    [tuts-apply-diff] Applying existing diff file for step validationgroups-add-create-group

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -19,7 +19,10 @@ use Symfony\Component\Validator\Constraints as Assert;
  *     accessControl="is_granted('ROLE_USER')",
  *     collectionOperations={
  *          "get",
- *          "post"={"access_control"="is_granted('IS_AUTHENTICATED_ANONYMOUSLY')"},
+ *          "post"={
+ *              "access_control"="is_granted('IS_AUTHENTICATED_ANONYMOUSLY')",
+ *              "validation_groups"={"Default", "create"}
+ *          },
  *     },
  *     itemOperations={
  *          "get",
@@ -79,7 +82,7 @@ class User implements UserInterface
     /**
      * @Groups("user:write")
      * @SerializedName("password")
-     * @Assert\NotBlank()
+     * @Assert\NotBlank(groups={"create"})
      */
     private $plainPassword;
 
