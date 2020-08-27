403f659fd1cb38b370cf7d2db257267cfc11ab95
Ryan Weaver <ryan@thatsquality.com>
2020-08-27 13:11:00 -0400

    [tuts-apply-diff] Applying existing diff file for step pre-ep3-updating-repositories

--- src/Repository/CheeseListingRepository.php
+++ src/Repository/CheeseListingRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\CheeseListing;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method CheeseListing|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class CheeseListingRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, CheeseListing::class);
     }
