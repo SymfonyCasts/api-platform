267380922770c651213eb5d33f06504be003f44a
Ryan Weaver <ryan@thatsquality.com>
2020-08-27 13:11:11 -0400

    [tuts-apply-diff] Applying existing diff file for step embeddednormalization-show-embedded

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -48,7 +48,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
-     * @Groups({"user:read", "user:write"})
+     * @Groups({"user:read", "user:write", "cheese:read"})
      * @Assert\NotBlank()
      * @Assert\Email()
      */
