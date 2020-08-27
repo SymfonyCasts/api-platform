fa86546fcc911b677e294a4790ac24f525c35cfe
Ryan Weaver <ryan@thatsquality.com>
2020-08-27 13:10:53 -0400

    [tuts-apply-diff] Applying existing diff file for step dynamserialization-add-admin-groups

--- src/Serializer/AdminGroupsContextBuilder.php
+++ src/Serializer/AdminGroupsContextBuilder.php
@@ -20,10 +20,11 @@ final class AdminGroupsContextBuilder implements SerializerContextBuilderInterfa
     public function createFromRequest(Request $request, bool $normalization, ?array $extractedAttributes = null): array
     {
         $context = $this->decorated->createFromRequest($request, $normalization, $extractedAttributes);
-        $resourceClass = $context['resource_class'] ?? null;
 
-        if ($resourceClass === Book::class && isset($context['groups']) && $this->authorizationChecker->isGranted('ROLE_ADMIN') && false === $normalization) {
-            $context['groups'][] = 'admin:input';
+        $isAdmin = $this->authorizationChecker->isGranted('ROLE_ADMIN');
+
+        if (isset($context['groups']) && $isAdmin) {
+            $context['groups'][] = $normalization ? 'admin:read' : 'admin:write';
         }
 
         return $context;
