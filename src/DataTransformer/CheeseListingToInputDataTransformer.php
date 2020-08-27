c6be9db2be9795cd0090b4ebde2003a187426790
Ryan Weaver <ryan@thatsquality.com>
2020-08-27 13:53:43 -0400

    tuts hero working...

--- src/DataTransformer/CheeseListingToInputDataTransformer.php
+++ src/DataTransformer/CheeseListingToInputDataTransformer.php
@@ -3,17 +3,23 @@
 namespace App\DataTransformer;
 
 use ApiPlatform\Core\DataTransformer\DataTransformerInterface;
+use App\Dto\CheeseListingInput;
+use App\Entity\CheeseListing;
 
 class CheeseListingToInputDataTransformer implements DataTransformerInterface
 {
     public function transform($object, string $to, array $context = [])
     {
+        dump($object);
     }
 
     public function supportsTransformation($data, string $to, array $context = []): bool
     {
-        dump($data, $to, $context);
+        if ($data instanceof CheeseListing) {
+            // already transformed
+            return false;
+        }
 
-        return false;
+        return $to === CheeseListing::class && ($context['input']['class'] ?? null) === CheeseListingInput::class;
     }
 }
