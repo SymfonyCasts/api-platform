bcd44252b2a08c4c1b6b38f37f78cff6682b8f1a
Ryan Weaver <ryan@thatsquality.com>
2020-08-27 13:10:36 -0400

    [tuts-apply-diff] Applying existing diff file for step user-fix-getusername

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -62,7 +62,7 @@ class User implements UserInterface
      */
     public function getUsername(): string
     {
-        return (string) $this->email;
+        return (string) $this->username;
     }
 
     /**
