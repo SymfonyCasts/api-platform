068e826e45fa58c89ae01fc75e1dc300e623151a
Ryan Weaver <ryan@thatsquality.com>
2020-08-27 13:10:58 -0400

    [tuts-apply-diff] Applying existing diff file for step extension-add-admin-exception

--- src/ApiPlatform/CheeseListingIsPublishedExtension.php
+++ src/ApiPlatform/CheeseListingIsPublishedExtension.php
@@ -6,12 +6,24 @@ use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInter
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use App\Entity\CheeseListing;
 use Doctrine\ORM\QueryBuilder;
+use Symfony\Component\Security\Core\Security;
 
 class CheeseListingIsPublishedExtension implements QueryCollectionExtensionInterface
 {
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
     {
         if ($resourceClass !== CheeseListing::class) {
+            return;
+        }
+
+        if ($this->security->isGranted('ROLE_ADMIN')) {
             return;
         }
 
