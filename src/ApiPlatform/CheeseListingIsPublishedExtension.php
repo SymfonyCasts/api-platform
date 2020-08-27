f1c35744c2c28ac9c924433de3ba084e8682b478
Ryan Weaver <ryan@thatsquality.com>
2020-08-27 13:10:58 -0400

    [tuts-apply-diff] Applying existing diff file for step extension-add-ispublished-where

--- src/ApiPlatform/CheeseListingIsPublishedExtension.php
+++ src/ApiPlatform/CheeseListingIsPublishedExtension.php
@@ -4,12 +4,19 @@ namespace App\ApiPlatform;
 
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use App\Entity\CheeseListing;
 use Doctrine\ORM\QueryBuilder;
 
 class CheeseListingIsPublishedExtension implements QueryCollectionExtensionInterface
 {
     public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
     {
+        if ($resourceClass !== CheeseListing::class) {
+            return;
+        }
 
+        $rootAlias = $queryBuilder->getRootAliases()[0];
+        $queryBuilder->andWhere(sprintf('%s.isPublished = :isPublished', $rootAlias))
+            ->setParameter('isPublished', true);
     }
 }
