0eb5d9165bdac8cdf95268473ba4094218942d9d
Ryan Weaver <ryan@thatsquality.com>
2020-08-27 13:10:59 -0400

    [tuts-apply-diff] Applying existing diff file for step relation-add-failing-test

--- tests/Functional/CheeseListingResourceTest.php
+++ tests/Functional/CheeseListingResourceTest.php
@@ -108,7 +108,7 @@ class CheeseListingResourceTest extends CustomApiTestCase
     public function testGetCheeseListingItem()
     {
         $client = self::createClient();
-        $user = $this->createUser('cheeseplese@example.com', 'foo');
+        $user = $this->createUserAndLogIn($client, 'cheeseplese@example.com', 'foo');
 
         $cheeseListing1 = new CheeseListing('cheese1');
         $cheeseListing1->setOwner($user);
@@ -122,5 +122,9 @@ class CheeseListingResourceTest extends CustomApiTestCase
 
         $client->request('GET', '/api/cheeses/'.$cheeseListing1->getId());
         $this->assertResponseStatusCodeSame(404);
+
+        $client->request('GET', '/api/users/'.$user->getId());
+        $data = $client->getResponse()->toArray();
+        $this->assertEmpty($data['cheeseListings']);
     }
 }
