af8ff87535612b78c15fb961758c544c5014d56f
Ryan Weaver <ryan@thatsquality.com>
2020-08-27 13:10:36 -0400

    [tuts-apply-diff] Applying existing diff file for step user-make-entity-add-username

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -33,6 +33,11 @@ class User implements UserInterface
      */
     private $password;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -109,5 +114,12 @@ class User implements UserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
     }
 }
