c2e3d89b0528c27013a03cf6dbfdf0cc5e2809e5
Ryan Weaver <ryan@thatsquality.com>
2020-08-27 13:10:35 -0400

    [tuts-apply-diff] Applying existing diff file for step pagination-add-pagination-items-per-page

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -22,7 +22,10 @@ use Symfony\Component\Serializer\Annotation\SerializedName;
  *     },
  *     shortName="cheeses",
  *     normalizationContext={"groups"={"cheese_listing:read"}, "swagger_definition_name"="Read"},
- *     denormalizationContext={"groups"={"cheese_listing:write"}, "swagger_definition_name"="Write"}
+ *     denormalizationContext={"groups"={"cheese_listing:write"}, "swagger_definition_name"="Write"},
+ *     attributes={
+ *          "pagination_items_per_page"=10
+ *     }
  * )
  * @ApiFilter(BooleanFilter::class, properties={"isPublished"})
  * @ApiFilter(SearchFilter::class, properties={"title": "partial", "description": "partial"})
