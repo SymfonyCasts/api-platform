1430c425f6ee51ef88303c1f1d69128073df398b
Ryan Weaver <ryan@thatsquality.com>
2020-08-27 13:10:50 -0400

    [tuts-apply-diff] Applying existing diff file for step encoding-bootstrap-data-persister

---
+++ src/DataPersister/UserDataPersister.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DataPersister;
+
+use ApiPlatform\Core\DataPersister\DataPersisterInterface;
+
+class UserDataPersister implements DataPersisterInterface
+{
+    public function supports($data): bool
+    {
+        // TODO: Implement supports() method.
+    }
+
+    public function persist($data)
+    {
+        // TODO: Implement persist() method.
+    }
+
+    public function remove($data)
+    {
+        // TODO: Implement remove() method.
+    }
+}
