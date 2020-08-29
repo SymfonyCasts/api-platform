970078e28044649f1d770a77ac448f7a57901534
Ryan Weaver <ryan@thatsquality.com>
2020-08-29 18:16:58 -0400

    tuts hero working...

--- src/Dto/CheeseListingInput.php
+++ src/Dto/CheeseListingInput.php
@@ -58,6 +58,7 @@ class CheeseListingInput
     /**
      * The description of the cheese as raw text.
      *
+     * @param string $description
      * @Groups({"cheese:write", "user:write"})
      * @SerializedName("description")
      */
