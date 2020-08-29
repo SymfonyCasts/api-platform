fe9f81fc8455ddfe448d4faaed7718489e04b281
Ryan Weaver <ryan@thatsquality.com>
2020-08-29 17:46:52 -0400

    tuts hero working...

--- src/Serializer/Normalizer/CheeseListingInputDenormalizer.php
+++ src/Serializer/Normalizer/CheeseListingInputDenormalizer.php
@@ -6,15 +6,25 @@ use App\Dto\CheeseListingInput;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\Normalizer\CacheableSupportsMethodInterface;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
 class CheeseListingInputDenormalizer implements DenormalizerInterface, CacheableSupportsMethodInterface
 {
+    private $objectNormalizer;
+
+    public function __construct(ObjectNormalizer $objectNormalizer)
+    {
+        $this->objectNormalizer = $objectNormalizer;
+    }
+
     public function denormalize($data, string $type, string $format = null, array $context = [])
     {
         $dto = new CheeseListingInput();
         $dto->title = 'I am set in the denormalizer!';
 
         $context[AbstractNormalizer::OBJECT_TO_POPULATE] = $dto;
+
+        return $this->objectNormalizer->denormalize($data, $type, $format, $context);
     }
 
     public function supportsDenormalization($data, string $type, string $format = null)
