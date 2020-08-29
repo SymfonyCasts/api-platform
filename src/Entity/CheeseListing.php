656e86da834aa5bfc35746ca721247bc5be4d13e
Ryan Weaver <ryan@thatsquality.com>
2020-08-29 16:18:51 -0400

    tuts hero working...

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -67,7 +67,7 @@ class CheeseListing
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"cheese:read", "cheese:write", "user:read", "user:write"})
+     * @Groups({"cheese:read", "cheese:write", "user:write"})
      * @Assert\NotBlank()
      * @Assert\Length(
      *     min=2,
@@ -88,7 +88,7 @@ class CheeseListing
      * The price of this delicious cheese, in cents
      *
      * @ORM\Column(type="integer")
-     * @Groups({"cheese:read", "cheese:write", "user:read", "user:write"})
+     * @Groups({"cheese:read", "cheese:write", "user:write"})
      * @Assert\NotBlank()
      */
     private $price;
