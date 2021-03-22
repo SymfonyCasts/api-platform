3028e35a5e2b25d3f4adb73875f90b372125616e
Ryan Weaver <ryan@thatsquality.com>
2021-03-22 14:23:13 -0400

    [tuts-apply-diff] Applying existing diff file for step test-first-simple-test

---
+++ tests/Functional/CheeseListingResourceTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use App\ApiPlatform\Test\ApiTestCase;
+
+class CheeseListingResourceTest extends ApiTestCase
+{
+    public function testCreateCheeseListing()
+    {
+        $this->assertEquals(42, 42);
+    }
+}
