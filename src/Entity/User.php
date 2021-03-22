6cc72b33834b0cce3d9692992b3b949ecf66e600
Ryan Weaver <ryan@thatsquality.com>
2021-03-22 14:23:00 -0400

    [tuts-apply-diff] Applying existing diff file for step user-make-username-unique

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -34,7 +34,7 @@ class User implements UserInterface
     private $password;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
      */
     private $username;
 
