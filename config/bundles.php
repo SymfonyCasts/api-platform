9bbb4e4b73e8acc25805edcce3665cdd4afcda01
Ryan Weaver <ryan@thatsquality.com>
2021-03-22 14:22:50 -0400

    [tuts-apply-diff] Applying existing diff file for step first-resource-install-migrations

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
