c3cc4f42d9094013a2681261c18b541e6bd9b451
Ryan Weaver <ryan@thatsquality.com>
2021-03-22 14:23:22 -0400

    [tuts-apply-diff] Applying existing diff file for step validationgroups-add-test-for-update

--- tests/Functional/UserResourceTest.php
+++ tests/Functional/UserResourceTest.php
@@ -24,4 +24,20 @@ class UserResourceTest extends CustomApiTestCase
 
         $this->logIn($client, 'cheeseplease@example.com', 'brie');
     }
+
+    public function testUpdateUser()
+    {
+        $client = self::createClient();
+        $user = $this->createUserAndLogIn($client, 'cheeseplease@example.com', 'foo');
+
+        $client->request('PUT', '/api/users/'.$user->getId(), [
+            'json' => [
+                'username' => 'newusername'
+            ]
+        ]);
+        $this->assertResponseIsSuccessful();
+        $this->assertJsonContains([
+            'username' => 'newusername'
+        ]);
+    }
 }
