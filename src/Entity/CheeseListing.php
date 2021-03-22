b0f16f05f205cbcd6c5771c3af7e642e25cee776
Ryan Weaver <ryan@thatsquality.com>
2021-03-22 14:23:18 -0400

    [tuts-apply-diff] Applying existing diff file for step security-show-previous-object

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -21,7 +21,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  *              "normalization_context"={"groups"={"cheese_listing:read", "cheese_listing:item:get"}},
  *          },
  *          "put"={
- *              "access_control"="is_granted('ROLE_USER') and object.getOwner() == user",
+ *              "access_control"="is_granted('ROLE_USER') and previous_object.getOwner() == user",
  *              "access_control_message"="Only the creator can edit a cheese listing"
  *          },
  *          "delete"={"access_control"="is_granted('ROLE_ADMIN')"}
