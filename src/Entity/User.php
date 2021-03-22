09d07f93dce26ba667d6d56f68a61702cd92014d
Ryan Weaver <ryan@thatsquality.com>
2021-03-22 14:23:20 -0400

    [tuts-apply-diff] Applying existing diff file for step encoding-erasecredentials

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -160,7 +160,7 @@ class User implements UserInterface
     public function eraseCredentials()
     {
         // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
+        $this->plainPassword = null;
     }
 
     public function setUsername(string $username): self
