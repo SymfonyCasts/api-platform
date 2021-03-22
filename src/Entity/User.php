b4a219fb096dc1220615e73242b61602334d1a4e
Ryan Weaver <ryan@thatsquality.com>
2021-03-22 14:23:02 -0400

    [tuts-apply-diff] Applying existing diff file for step relation-expose-user-cheeselistings

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -58,6 +58,7 @@ class User implements UserInterface
 
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\CheeseListing", mappedBy="owner")
+     * @Groups("user:read")
      */
     private $cheeseListings;
 
