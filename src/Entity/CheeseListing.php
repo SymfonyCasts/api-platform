238d329943c63d5a0e4afa52fdab9119b32977ac
Ryan Weaver <ryan@thatsquality.com>
2021-03-22 14:22:56 -0400

    [tuts-apply-diff] Applying existing diff file for step serializetricks-serializedname

--- src/Entity/CheeseListing.php
+++ src/Entity/CheeseListing.php
@@ -6,6 +6,7 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use Carbon\Carbon;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\SerializedName;
 
 /**
  * @ApiResource(
@@ -97,6 +98,7 @@ class CheeseListing
      * The description of the cheese as raw text.
      *
      * @Groups("cheese_listing:write")
+     * @SerializedName("description")
      */
     public function setTextDescription(string $description): self
     {
