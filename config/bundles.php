068be92756c2789e8376b3d59308e51f28a9f311
Ryan Weaver <ryan@thatsquality.com>
2021-03-22 14:23:07 -0400

    [tuts-apply-diff] Applying existing diff file for step pre-ep2-install-encore

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
