2014f1108dbcb4479498f3082a10e61d8cddc232
Ryan Weaver <ryan@thatsquality.com>
2021-03-22 14:23:21 -0400

    [tuts-apply-diff] Applying existing diff file for step encoding-rename-field-to-password

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,6 +11,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\SerializedName;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
@@ -77,6 +78,7 @@ class User implements UserInterface
 
     /**
      * @Groups("user:write")
+     * @SerializedName("password")
      */
     private $plainPassword;
 
