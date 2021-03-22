71f26cb0fd131e3383fa695edf6565ef831e3bc9
Ryan Weaver <ryan@thatsquality.com>
2021-03-22 14:23:23 -0400

    [tuts-apply-diff] Applying existing diff file for step dynamserialization-chnage-to-admin-read

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -89,7 +89,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=50, nullable=true)
-     * @Groups({"user:read", "user:write"})
+     * @Groups({"admin:read", "user:write"})
      */
     private $phoneNumber;
 
